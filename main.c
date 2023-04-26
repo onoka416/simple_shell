d15aba92a5369603af19327e3666b3d7c721dd41
onoka416 <onokabilly@gmail.com>
2023-04-26 14:51:53 -0700

    shell

--- main.c
+++ main.c
@@ -1,55 +1,38 @@
+#include "parser.h"
 #include "shell.h"
-
 /**
- * main - Entry point for the shell program.
+ * The main entry into my shell
  *
- * @ac: Argument count.
- * @av: Array of strings containing arguments.
+ * @argc:  arguments that are received in shell
+ * @argv: arguments that are  received shell
  *
- * Return: Always 0.
- */
-int main(int ac, char **av)
+ * Returnig 0 on the success and 1 when errors occurs.
+ **/
+
+int main(int argc, char **argv)
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+    general_t *info;
+    int status_code;
 
-	/* Change file descriptor */
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+    info = malloc(sizeof(general_t));
+    if (info == NULL)
+    {
+        perror(argv[0]);
+        exit(1);
+    }
 
-	/* Open file if provided */
-	if (ac == 2)
-	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			/* Check file open error */
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->readfd = fd;
-	}
+    info->pid = getpid();
+    info->status_code = 0;
+    info->n_commands = 0;
+    info->argc = argc;
+    info->argv = argv;
+    info->mode = isatty(STDIN) == INTERACTIVE;
+    start(info);
 
-	/* Populate env list */
-	populate_env_list(info);
+    status_code = info->status_code;
 
-	/* Read history */
-	read_history(info);
+    free(info);
 
-	/* Launch the shell */
-	hsh(info, av);
-
-	return (EXIT_SUCCESS);
+    return (status_code);
 }
+
