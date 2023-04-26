5b8912389dfb47567fc983899bea6c2b39ef7702
onoka416 <onokabilly@gmail.com>
2023-04-26 15:28:12 -0700

    main1

--- main.c
+++ main.c
@@ -12,28 +12,28 @@
  **/
 int main(int argc, char **argv)
 {
-    general_t *info;
-    int status_code;
+	general_t *info;
+	int status_code;
 
-    info = malloc(sizeof(general_t));
-    if (info == NULL)
-    {
-        perror(argv[0]);
-        exit(1);
-    }
+	info = malloc(sizeof(general_t));
+	if (info == NULL)
+	{
+		perror(argv[0]);
+		exit(1);
+	}
 
-    info->pid = getpid();
-    info->status_code = 0;
-    info->n_commands = 0;
-    info->argc = argc;
-    info->argv = argv;
-    info->mode = isatty(STDIN) == INTERACTIVE;
-    start(info);
+	info->pid = getpid();
+	info->status_code = 0;
+	info->n_commands = 0;
+	info->argc = argc;
+	info->argv = argv;
+	info->mode = isatty(STDIN) == INTERACTIVE;
+	start(info);
 
-    status_code = info->status_code;
+	status_code = info->status_code;
 
-    free(info);
+	free(info);
 
-    return (status_code);
+	return (status_code);
 }
 
