516b0bc3b822b5f9474a05e7ea759f40076dad43
onoka416 <onokabilly@gmail.com>
2023-04-27 04:31:50 -0700

    my mainc

--- main.c
+++ main.c
@@ -9,54 +9,39 @@
  */
 int main(int ac, char **av)
 {
-	info_t info = INFO_INIT;
+/* This function is the entry point for the shell. */
+
+	info_t info[] = { INFO_INIT };
 	int fd = 2;
 
-	/* Check if there is only one argument */
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+ /* Check if there is only one argument. */
 	if (ac == 2)
 	{
-		/* Open the file specified in the argument */
+		/* Open the file specified in the argument. */
 		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
 		{
-			/* Check for errors */
 			if (errno == EACCES)
-			{
 				exit(126);
-			}
-			else if (errno == ENOENT)
+			if (errno == ENOENT)
 			{
 				_eputs(av[0]);
 				_eputs(": 0: Can't open ");
 				_eputs(av[1]);
-				_eputs(CRLF);
+				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
 				exit(127);
 			}
-			else
-			{
-				return (EXIT_FAILURE);
-			}
+			return (EXIT_FAILURE);
 		}
-		/* Set the file descriptor for reading */
-		info.readfd = fd;
+		info->readfd = fd;
 	}
-	else
-	{
-		/* If there are more than one argument, print an error message and exit */
-		_eputs("Usage: ");
-		_eputs(av[0]);
-		_eputs(" [filename]\n");
-		exit(127);
-	}
-
-	/* Populate the environment list */
-	populate_env_list(&info);
-	/* Read the history */
-	read_history(&info);
-	/* Run the shell */
-	hsh(&info, av);
-
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
-
