492220cb0e56d3f3d7ebe3e936e1a0b2b7995c7a
onoka416 <onokabilly@gmail.com>
2023-04-27 07:27:19 -0700

    the getenv.c

--- getenv.c
+++ getenv.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * get_environ - returns the string array copy of our environ
- * @info: Structure containing potential arguments. Used to maintain
+ * getting the _environ - returns the string array copy of our environ
+ * @info:The  Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  * Return: Always 0
  */
