52684680e01bc6d7d92226481c5cd3c90e6edf8c
onoka416 <onokabilly@gmail.com>
2023-04-27 06:31:04 -0700

    my updated main

--- main.c
+++ main.c
@@ -1,44 +1,55 @@
 #include "shell.h"
 
 /**
- * the main - entry point
- * @ac: argiments  counts
- * @av: arumentsgs vector
+ * main - Entry point of the shell program
+ * @argc: The number of arguments
+ * @argv: An array of strings containing the arguments
  *
- * Return: 0 on success, 1 on error
+ * Return: Always 0
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+    /* Initialize a struct containing information about the shell */
+    shell_info_t shell_info[] = { SHELL_INFO_INIT };
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+    int file_descriptor = 2;
 
-	if (ac == 2)
-	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->readfd = fd;
-	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
+    /* Inline assembly to add 3 to the file descriptor */
+    asm ("mov %1, %0\n\t"
+         "add $3, %0"
+         : "=r" (file_descriptor)
+         : "r" (file_descriptor));
+
+    /* If an input file is provided, open it for reading */
+    if (argc == 2)
+    {
+        file_descriptor = open(argv[1], O_RDONLY);
+        if (file_descriptor == -1)
+        {
+            /* Handle errors when opening the file */
+            if (errno == EACCES)
+                exit(126);
+            if (errno == ENOENT)
+            {
+                _eputs(argv[0]);
+                _eputs(": 0: Can't open ");
+                _eputs(argv[1]);
+                _eputchar('\n');
+                _eputchar(BUF_FLUSH);
+                exit(127);
+            }
+            return (EXIT_FAILURE);
+        }
+        /* Set the read file descriptor to the opened file */
+        shell_info->readfd = file_descriptor;
+    }
+    /* Populate the environment list */
+    populate_env_list(shell_info);
+    /* Read command history */
+    read_history(shell_info);
+    /* Start the shell program */
+    start_shell(shell_info, argv);
+
+    return (EXIT_SUCCESS);
 }
+
