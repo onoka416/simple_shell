2f200244e57ec916dab128cbfd79e4e01f809aca
onoka416 <onokabilly@gmail.com>
2023-04-27 07:04:43 -0700

    history

--- history.c
+++ history.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * get_history_file - gets the history file
+ * getting the_history_file - gets the history file
  * @info: parameter struct
  *
  * Return: allocated string containg history file
