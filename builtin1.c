704c784ef5e0d9b657833475ac273e33103d1e4d
onoka416 <onokabilly@gmail.com>
2023-04-27 07:19:38 -0700

    the builtin1 function

--- builtin1.c
+++ builtin1.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * _myhistory - displays the history list, one command by line, preceded
+ *My function  _myhistory - displays the history list, one command by line, preceded
  *              with line numbers, starting at 0.
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
