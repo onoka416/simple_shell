18cbc39f41319919102c9eb4c58908ac1e4b317a
Billy Onoka <59770528+onoka416@users.noreply.github.com>
2023-04-27 23:39:01 +0300

    atoi

--- _atoi.c
+++ _atoi.c
@@ -9,30 +9,23 @@
  */
 int main(int ac, char **av)
 {
-	// Initialize an info_t array with the INFO_INIT macro.
-	info_t info[] = { INFO_INIT };
-	// Initialize a file descriptor to 2, which represents the stderr stream.
+	info_t info[] = {INFO_INIT};
 	int fd = 2;
 
-	// Use inline assembly to modify the value of fd.
-	// This adds 3 to the value of fd, so it now represents the stdout stream.
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
-	
-	// If the program was called with two arguments, try to open the file specified by the second argument.
+	asm("mov %1, %0\n\t"
+	    "add $3, %0"
+	    : "=r"(fd)
+	    : "r"(fd));
+
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
 		{
-			// If the file could not be opened, check the value of errno to determine the type of error.
 			if (errno == EACCES)
-				exit(126); // Exit with a code of 126 to indicate a permission error.
+				exit(126);
 			if (errno == ENOENT)
 			{
-				// If the file was not found, print an error message and exit with an exit code of 127.
 				_eputs(av[0]);
 				_eputs(": 0: Can't open ");
 				_eputs(av[1]);
@@ -40,20 +33,12 @@ int main(int ac, char **av)
 				_eputchar(BUF_FLUSH);
 				exit(127);
 			}
-			// If an error other than permission or file not found occurred, return EXIT_FAILURE.
 			return (EXIT_FAILURE);
 		}
-		// If the file was successfully opened, set the readfd field of the info_t array to the file descriptor.
 		info->readfd = fd;
 	}
-	
-	// Populate a linked list of environment variables.
 	populate_env_list(info);
-	// Read in the shell's history file.
 	read_history(info);
-	// Call the hsh() function, passing in the info_t array and the argument vector.
 	hsh(info, av);
-	// Return the value returned by hsh(), which is either EXIT_SUCCESS or EXIT_FAILURE.
 	return (EXIT_SUCCESS);
 }
-
