f00ccf742c8b03cd85b9ca2be6375b5a8cb8aadf
onoka416 <onokabilly@gmail.com>
2023-04-27 08:17:54 -0700

    the builtin errors

--- builtin.c
+++ builtin.c
@@ -1,17 +1,17 @@
 #include "shell.h"
 
 /**
- * the _myexit -this  exits the shell
+ * _myexit - exits the shell
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
- *  Returns the with the  exits with a given exit status
+ *  Return: exits with a given exit status
  *         (0) if info.argv[0] != "exit"
  */
 int _myexit(info_t *info)
 {
 	int exitcheck;
 
-	if (info->argv[1])  /* Incase there is  is an exit arguement */
+	if (info->argv[1])  /* If there is an exit arguement */
 	{
 		exitcheck = _erratoi(info->argv[1]);
 		if (exitcheck == -1)
@@ -30,7 +30,7 @@ int _myexit(info_t *info)
 }
 
 /**
- *the  _mycd - changes the current directory of the process
+ * _mycd - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
@@ -95,3 +95,4 @@ int _myhelp(info_t *info)
 		_puts(*arg_array); /* temp att_unused workaround */
 	return (0);
 }
+
