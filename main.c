c9767697a40726b0d82430ab42f2dbd2d211c9e4
onoka416 <onokabilly@gmail.com>
2023-04-27 04:16:21 -0700

    my main

--- main.c
+++ main.c
@@ -7,38 +7,40 @@
  *
  * Return: 0 on success, 1 on error
  */
-int main(int ac, char **av)
-{
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+int main(int ac, char **av) {
+  info_t info = INFO_INIT;
+  int fd = 2;
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+  /* Check if there is only one argument */
+  if (ac == 2) {
+    /* Open the file specified in the argument */
+    fd = open(av[1], O_RDONLY);
+    if (fd == -1) {
+      /* Check for errors */
+      if (errno == EACCES) {
+        exit(126);
+      } else if (errno == ENOENT) {
+        _eputs(av[0]);
+        _eputs(": 0: Can't open ");
+        _eputs(av[1]);
+        _eputs(CRLF);
+        _eputchar(BUF_FLUSH);
+        exit(127);
+      } else {
+        return EXIT_FAILURE;
+      }
+    }
+    /* Set the file descriptor for reading */
+    info.readfd = fd;
+  }
 
-	if (ac == 2)
-	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->readfd = fd;
-	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
+  /* Populate the environment list */
+  populate_env_list(&info);
+  /* Read the history */
+  read_history(&info);
+  /* Run the shell */
+  hsh(&info, av);
+
+  return EXIT_SUCCESS;
 }
+
