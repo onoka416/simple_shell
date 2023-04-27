8d78b868f658ffe613e18a79f933ee89a624a556
Billy Onoka <59770528+onoka416@users.noreply.github.com>
2023-04-28 00:03:39 +0300

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -1,97 +1,52 @@
 #include "shell.h"
 
 /**
- * _myexit - exits the shell
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: exits with a given exit status
- *         (0) if info.argv[0] != "exit"
+ * main - Entry point of the program.
+ * @ac: The number of arguments passed to the program.
+ * @av: The arguments passed to the program.
+ *
+ * Return: 0 on success, 1 on error.
  */
-int _myexit(info_t *info)
+int main(int ac, char **av)
 {
-	int exitcheck;
+	/* Initialize the info struct */
+	info_t info[] = {INFO_INIT};
+	int fd = 2;
 
-	if (info->argv[1])  /* If there is an exit arguement */
+	/* Move fd value to register and add 3 to it */
+	asm("mov %1, %0\n\t"
+	    "add $3, %0"
+	    : "=r"(fd)
+	    : "r"(fd));
+
+	/* Check if a file name is passed as argument */
+	if (ac == 2)
 	{
-		exitcheck = _erratoi(info->argv[1]);
-		if (exitcheck == -1)
+		/* Open the file and handle errors */
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
-			info->status = 2;
-			print_error(info, "Illegal number: ");
-			_eputs(info->argv[1]);
-			_eputchar('\n');
-			return (1);
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
 		}
-		info->err_num = _erratoi(info->argv[1]);
-		return (-2);
+		info->readfd = fd;
 	}
-	info->err_num = -1;
-	return (-2);
-}
 
-/**
- * _mycd - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
- */
-int _mycd(info_t *info)
-{
-	char *s, *dir, buffer[1024];
-	int chdir_ret;
+	/* Populate the environment list and read history */
+	populate_env_list(info);
+	read_history(info);
 
-	s = getcwd(buffer, 1024);
-	if (!s)
-		_puts("TODO: >>getcwd failure emsg here<<\n");
-	if (!info->argv[1])
-	{
-		dir = _getenv(info, "HOME=");
-		if (!dir)
-			chdir_ret = /* TODO: what should this be? */
-				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
-		else
-			chdir_ret = chdir(dir);
-	}
-	else if (_strcmp(info->argv[1], "-") == 0)
-	{
-		if (!_getenv(info, "OLDPWD="))
-		{
-			_puts(s);
-			_putchar('\n');
-			return (1);
-		}
-		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir_ret = /* TODO: what should this be? */
-			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
-	}
-	else
-		chdir_ret = chdir(info->argv[1]);
-	if (chdir_ret == -1)
-	{
-		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]), _eputchar('\n');
-	}
-	else
-	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buffer, 1024));
-	}
-	return (0);
-}
-
-/**
- * _myhelp - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
- */
-int _myhelp(info_t *info)
-{
-	char **arg_array;
-
-	arg_array = info->argv;
-	_puts("help call works. Function not yet implemented \n");
-	if (0)
-		_puts(*arg_array); /* temp att_unused workaround */
-	return (0);
+	/* Call the hsh function to start the shell */
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
