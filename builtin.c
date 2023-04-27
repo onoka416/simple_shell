e9d5b10d8e5d081c4ef581f1c381a2756532b2fe
onoka416 <onokabilly@gmail.com>
2023-04-27 06:42:46 -0700

    bulletin

--- builtin.c
+++ builtin.c
@@ -4,14 +4,14 @@
  * the _myexit -this  exits the shell
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
- *  Return: exits with a given exit status
+ *  Returns the with the  exits with a given exit status
  *         (0) if info.argv[0] != "exit"
  */
 int _myexit(info_t *info)
 {
 	int exitcheck;
 
-	if (info->argv[1])  /* If there is an exit arguement */
+	if (info->argv[1])  /* Incase there is  is an exit arguement */
 	{
 		exitcheck = _erratoi(info->argv[1]);
 		if (exitcheck == -1)
@@ -30,7 +30,7 @@ int _myexit(info_t *info)
 }
 
 /**
- * _mycd - changes the current directory of the process
+ *the  _mycd - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
